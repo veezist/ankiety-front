f6b782a5736fc2f450a11a9870f4b4368c718b1f
Oskar Dadan <skrzynkanof@gmail.com>
2019-02-24 15:47:21 +0100

    home page error fix

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -29,7 +29,7 @@ export interface Tile {
 })
 export class HomeComponent implements OnInit
 {
-  surveys: Object;
+  surveys: Object = undefined;
   constructor(private data: DataService) {}
 
   ngOnInit()
