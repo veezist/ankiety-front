66f23a129120d4fea908f40f335c783a1601cfd5
Oskar Dadan <skrzynkanof@gmail.com>
2019-02-24 13:20:14 +0100

    work on data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService
 {
+  private baseUrl = '';
+
   constructor(private http: HttpClient) { }
 
   getFakeSurveys()
   {
     return this.http.get('http://www.mocky.io/v2/5c34b1b92e00008200379039')
+  }
+
+  public getObjectByUrl<T>(type: new () => T, relativeUrl: string): Observable<T>
+  {
+    const url = `${this.baseUrl}${relativeUrl}`;
+    return this.http.get<T>(url).pipe(
+      // catchError()
+    );
   }
 }
