45ac526df3985ebfb1fb0deff8dfe9078251c49a
Oskar Dadan <skrzynkanof@gmail.com>
2019-04-14 13:14:13 +0200

    scroll to circles

--- src/app/pages/home/segments/header-segment/header-segment.component.ts
+++ src/app/pages/home/segments/header-segment/header-segment.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header-segment',
-  templateUrl: './header-segment.component.html',
-  styleUrls: ['./header-segment.component.scss']
+	selector: 'app-header-segment',
+	templateUrl: './header-segment.component.html',
+	styleUrls: ['./header-segment.component.scss']
 })
-export class HeaderSegmentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class HeaderSegmentComponent implements OnInit
+{
+	constructor() { }
+	
+	ngOnInit()
+	{
+	}
+	
+	scrollToCircles()
+	{
+		document.getElementsByTagName("app-circles-segment").item(0).scrollIntoView({ behavior: "smooth" });
+	}
 }
