e947ad60d047f827b7e35735ae6342d7d25b2c37
Oskar Dadan <skrzynkanof@gmail.com>
2019-05-02 00:28:03 +0200

    user-panel and profile added to navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { EmailChangeComponent } from './pages/profile/email-change/email-change.component';
 import { PasswordChangeComponent } from './pages/profile/password-change/password-change.component';
 import { SurveyFillComponent } from './pages/survey-fill/survey-fill.component';
+import { UserPanelComponent } from './pages/user-panel/user-panel.component';
 
 const routes: Routes = [
    { path: 'creator', component: SurveyCreatorComponent }
@@ -21,6 +22,7 @@ const routes: Routes = [
   ,{ path: 'email_change', component: EmailChangeComponent}
   ,{ path: 'password_change', component: PasswordChangeComponent}
   ,{ path: 'profile', component: ProfileComponent }
+  ,{ path: 'user-panel', component: UserPanelComponent }
   ,{ path: 'survey/:id', component: SurveyFillComponent }
   ,{ path: '', component: HomeComponent }
   ,{ path: '**', redirectTo: '', pathMatch: 'full'
