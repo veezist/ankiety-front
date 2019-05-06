7c226e4a692c6ab29155b5c0dfaa3a57fb568ce6
ajgorw <wojtowicz.igor@gmail.com>
2019-05-06 12:15:15 +0200

    Circle segment modificated. This commit include bug in first circle. After secound time on mouse hover circle text are changing

--- src/app/pages/home/segments/circles-segment/circles-segment.component.ts
+++ src/app/pages/home/segments/circles-segment/circles-segment.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router'
 @Component({
   selector: 'app-circles-segment',
   templateUrl: './circles-segment.component.html',
-  styleUrls: ['./circles-segment.component.scss']
+  styleUrls: ['./circles-segment.component.scss'],
+
 })
 export class CirclesSegmentComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router: Router, private route: ActivatedRoute) { }
+  
   ngOnInit() {
+    var el = document.getElementById("firstCircle");
+    el.textContent = "1000 stworzonych ankiet"
   }
-
+  
+  mouseHoverFirstCircle(e) {
+    var el = document.getElementById("firstCircle");
+    var lastText = el.textContent
+    el.textContent = ""
+    if (lastText == "1000 stworzonych ankiet") {
+      el.textContent = "Przejrzyj ankiety";
+    } else {
+      el.textContent = "1000 stworzonych ankiet";
+  }
+  }
+  onFirstCircleClick()
+  {
+    var el = document.getElementById("firstCircle");
+    if(el.textContent = "Przejrzyj ankiety")
+    {
+      this.router.navigate([`../surveys/`], { relativeTo: this.route });
+    }
+  }
+   async delay(ms: number) {
+    await new Promise(resolve => setTimeout(()=>resolve(), ms)).then(()=>console.log("fired"));
+  }
 }
