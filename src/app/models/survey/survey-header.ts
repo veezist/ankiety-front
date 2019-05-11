e89fcf51d980979e15b2cb9fcfb4896f788d4bee
Oskar Dadan <skrzynkanof@gmail.com>
2019-05-11 21:59:54 +0200

    survey header model filled

--- src/app/models/survey/survey-header.ts
+++ src/app/models/survey/survey-header.ts
@@ -1,2 +1,11 @@
-export class SurveyHeader {
+export class SurveyHeader
+{
+	constructor
+	(
+		public id: number,
+		public title: string,
+		public description: string,
+		public countOfQuestion:number,
+		public userDisplay:string
+	) { }
 }
