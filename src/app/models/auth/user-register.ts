9acac00d74cc43dae343469e01bfd190fce570c6
Oskar Dadan <skrzynkanof@gmail.com>
2019-05-16 16:19:38 +0200

    register model changed

--- src/app/models/auth/user-register.ts
+++ src/app/models/auth/user-register.ts
@@ -1,11 +1,13 @@
 export class UserRegister
 {
+	private displayName: string;
+
 	constructor 
 	(
 		public name: string,
 		public surname: string,
 		public password: string,
 		public email: string,
-		public dateOfDeletion: Date
-	) { };
+		public dateOfDeletion: Date,
+	) { this.displayName = name + surname };
 }
